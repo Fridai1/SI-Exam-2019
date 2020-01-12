7638f0e5660df061ca0bd4988bc0e9d98611173d
Nikolai <nikolaiviby3@gmail.com>
2020-01-12 21:22:26 +0100

    added the logging project and tested that it worked

---
+++ src/services/LoggingService/LoggingService/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace LoggingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log logs = new Log();
+            var factory = new ConnectionFactory() { HostName = "192.168.0.25" };
+            factory.UserName = "admin";
+            factory.Password = "admin";
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                    exchange: "logs",
+                    routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    string[] messageArray = message.Split(' ');
+                    // starting a new thread, allowing for faster processing to avoid queue overflow.
+                    Task.Run(() => logs.CreateLog(messageArray));
+                };
+                channel.BasicConsume(queue: queueName,
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
